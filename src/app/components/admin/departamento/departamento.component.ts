9d130677f306a99a1ab2e703dfa9f40c3173e678
joi <joi.fogaca@gmail.com>
2019-12-20 13:55:24 -0300

    Programação da departamento.ts

--- src/app/components/admin/departamento/departamento.component.ts
+++ src/app/components/admin/departamento/departamento.component.ts
@@ -1,4 +1,10 @@
+
+import { DepartamentoService } from './../../../services/departamento.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Departamento } from 'src/app/models/departamento.model';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-departamento',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartamentoComponent implements OnInit {
 
-  constructor() { }
+  departamento$: Observable<Departamento[]>;
+  edit: boolean;
+  displayDialogDepartamento: boolean;
+  form: FormGroup;
+  constructor(private departamentoService:DepartamentoService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.departamento$ = this.departamentoService.list();
+    this.configForm();
+  }
+
+  configForm(){
+    this.form = this.fb.group(
+      {
+        id: new FormControl(),
+        nome: new FormControl('',Validators.required),
+        telefone: new FormControl('')
+      }
+    )
+  }
+
+  add(){
+    this.form.reset();
+    this.edit = false;
+    this.displayDialogDepartamento = true;
+  }
+
+  selecionaDepartamento(depto: Departamento){
+    this.edit = true;
+    this.displayDialogDepartamento = true;
+    this.form.setValue(depto);
+  }
+
+  save(){
+    this.departamentoService.createOrUpdate(this.form.value)
+    .then(() => {
+      this.displayDialogDepartamento = false;
+      Swal.fire(`Departamento 
+      ${ !this.edit ? 'salvo' : 'atualizado' } com sucesso.`, '', 'success')
+      .catch((erro) =>{
+        this.displayDialogDepartamento = false;
+        Swal.fire(`Erro ao ${ !this.edit ? 'salvo' : 'atualizado'} o departamento`,
+        `Detales: ${erro}`, 'error');
+        this.form.reset();
+
+      })
+    })
   }
 
 }
